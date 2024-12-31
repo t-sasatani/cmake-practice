a4d6e77d182ad01d42b508e259d89c40418a71e0
t-sasatani <sasatani.dev@gmail.com>
2024-12-31 22:51:05 +0900

    basic cmake structure

---
+++ build/src/CMakeFiles/greetings.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greetings.dir/good_morning.cpp.o"
+  "CMakeFiles/greetings.dir/good_morning.cpp.o.d"
+  "CMakeFiles/greetings.dir/hello.cpp.o"
+  "CMakeFiles/greetings.dir/hello.cpp.o.d"
+  "libgreetings.dylib"
+  "libgreetings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greetings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
