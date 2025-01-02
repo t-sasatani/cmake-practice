ceebc90436d447030dff5e90c37a008eab1d0a0f
t-sasatani <sasatani.dev@gmail.com>
2025-01-02 17:37:42 +0900

    Ignore test build artifact

--- LED_driver/build_test/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/takuya/Git/cmake-practice/LED_driver/test
-# Build directory: /Users/takuya/Git/cmake-practice/LED_driver/build_test/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LEDDriverTest "LEDDriverTest")
-set_tests_properties(LEDDriverTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/takuya/Git/cmake-practice/LED_driver/test/CMakeLists.txt;22;add_test;/Users/takuya/Git/cmake-practice/LED_driver/test/CMakeLists.txt;0;")
